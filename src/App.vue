89f97f3a2f079c310ebf81b0f31d68cf4bf73e8f
Konstantin Golubkin <rucoalsu.novokuznetsk@sgs.com>
2019-06-25 10:10:47 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+@import './style/CorpStyle/CorpV2.css';
+@import './style/CorpStyle/SGSRapidFramework.css';
+@import './style/CorpStyle/jq_ui_1.8.6.css';
+@import './style/MyExtStyle.css';
+</style>
