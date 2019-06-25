560ab8b333b5180faf76d164356f7a46f451ab62
Konstantin Golubkin <rucoalsu.novokuznetsk@sgs.com>
2019-06-25 14:07:26 +0700

    jq-ui

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
     }
   },
   module: {
@@ -88,5 +89,5 @@ module.exports = {
     net: 'empty',
     tls: 'empty',
     child_process: 'empty'
-  }
+  },
 }
