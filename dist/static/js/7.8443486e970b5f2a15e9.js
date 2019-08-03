e95b8f5124feddbdc70ff2686694dbda251b616e
Kahore <konstantin.golubkin@hotmail.com>
2019-08-03 20:11:19 +0800

    dist for heroku

---
+++ dist/static/js/7.8443486e970b5f2a15e9.js
@@ -0,0 +1,2 @@
+webpackJsonp([7],{oqGE:function(e,t,s){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var i={props:{external:{type:String,default:"list-ring"}}},n={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"lds-ring",class:this.external},[t("div"),this._v(" "),t("div"),this._v(" "),t("div"),this._v(" "),t("div")])},staticRenderFns:[]},l=s("VU/8")(i,n,!1,null,null,null);t.default=l.exports}});
+//# sourceMappingURL=7.8443486e970b5f2a15e9.js.map
